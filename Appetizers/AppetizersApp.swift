6de7de6f6db93103e510e710895411af74d4bea2
Michał Konkel <Michal.Konkel@velobank.pl>
2024-10-01 06:16:49 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Michal Konkel on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
